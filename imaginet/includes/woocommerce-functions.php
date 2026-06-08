0839d1e54f76dcbb6efb8318ae8f0d5378a22dd5
Roy <roy@imaginet.co.il>
2026-06-08 11:09:40 +0300

    Enhance WooCommerce theme support settings
    Updated WooCommerce support function to include additional theme support options for product images and galleries.

--- imaginet/includes/woocommerce-functions.php
+++ imaginet/includes/woocommerce-functions.php
@@ -1,8 +1,23 @@
 <?php
-add_action( 'after_setup_theme', 'woocommerce_support' );
-function woocommerce_support() {
-   add_theme_support( 'woocommerce' );
-} 
+function imaginet_woocommerce_support() {
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 300,
+        'single_image_width'    => 600,
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 1,
+            'max_rows'        => 6,
+            'default_columns' => 4,
+            'min_columns'     => 1,
+            'max_columns'     => 6,
+        ),
+    ) );
+
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+}
+add_action( 'after_setup_theme', 'imaginet_woocommerce_support' ); 
 
 //------------- Remove product tabs ---------------------//
 
